5083faecd522b3d46a5d23c9826c524f9c5a082f
JonathanFL <j.lydemann@gmail.com>
2019-02-12 10:36:31 +0100

    Har 2 coloumns i et grid og kan sætte decades ind i den ene, men mangler at kunne se navne og rankings for decades i den anden column.

--- BabyNames/BabyNames/MainWindow.xaml.cs
+++ BabyNames/BabyNames/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BabyNames
 {
@@ -20,9 +10,61 @@ namespace BabyNames
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<BabyName> _babyNames = new List<BabyName>();
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1900; i < 2000; i+=10)
+            {
+                ListBoxItem decadeItem = new ListBoxItem();
+                decadeItem.Content = i;
+                DecadeListBox.Items.Add(decadeItem);
+            }
+        }
+
+        private void BabyNameListBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = ItemsControl.ContainerFromElement(sender as ListBox, e.OriginalSource as DependencyObject) as ListBoxItem;
+            if (item != null)
+            {
+                // ListBox item clicked - do some cool things here
+                if (item.Content.Equals("2000"))
+                {
+                    ListBoxItem listBoxItem = new ListBoxItem();
+                    listBoxItem.Content = 
+                }
+            }
         }
     }
 }
+
+/*
+ * try
+            {
+                using (var sr = new StreamReader("BabyNames.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        var instans = new BabyName(line);
+                        _babyNames.Add(instans);
+                        //var item = new ListBoxItem();
+                        //item.Content = line;
+                        //ResultListBox.Items.Add(item);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                var item = new ListBoxItem
+                {
+                    Content = ex.Message
+                };
+                BabyNameListBox.Items.Add(item);
+            }
+ */
