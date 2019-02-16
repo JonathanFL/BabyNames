d5d6c6d327b1d3d9fcc7d07fe3420390d40f02bd
JonathanFL <j.lydemann@gmail.com>
2019-02-16 18:03:44 +0100

    Delopgave 3 duncan.

--- BabyNames/BabyNames/MainWindow.xaml.cs
+++ BabyNames/BabyNames/MainWindow.xaml.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace BabyNames
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[,] _rankMatrix = new string[2001, 10];
         private List<BabyName> _babyNames = new List<BabyName>();
         public MainWindow()
         {
@@ -19,43 +24,59 @@ namespace BabyNames
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            for (int i = 1900; i < 2000; i+=10)
-            {
-                ListBoxItem decadeItem = new ListBoxItem();
-                decadeItem.Content = i;
-                DecadeListBox.Items.Add(decadeItem);
-            }
-        }
+            for (int i = 1900; i <= 2000; i+=10)
+                DecadeListBox.Items.Add(i);
 
-        private void BabyNameListBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            var item = ItemsControl.ContainerFromElement(sender as ListBox, e.OriginalSource as DependencyObject) as ListBoxItem;
-            if (item != null)
+            AddBabyNamesToArray();
+
+            foreach (BabyName babyName in _babyNames)
             {
-                // ListBox item clicked - do some cool things here
-                if (item.Content.Equals("2000"))
+                for (int decade = 1900; decade <= 2000; decade += 10)
                 {
-                    ListBoxItem listBoxItem = new ListBoxItem();
-                    listBoxItem.Content = 
+                    int rank = babyName.Rank(decade);
+                    int decadeIndex = decade;
+                    if (0 < rank && rank <= 10)
+                        if (_rankMatrix[decadeIndex, rank - 1] == null)
+                        {
+                            _rankMatrix[decadeIndex, rank - 1] = babyName.Name;
+                        }
+                        else
+                        {
+                            _rankMatrix[decadeIndex, rank - 1] += " and " + babyName.Name;
+                        }
                 }
             }
         }
-    }
-}
 
-/*
- * try
+        private void DecadeListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BabyNameListBox.Items.Clear();
+            object selectedItem = DecadeListBox.SelectedItem;
+            ListBoxItem selectedListBoxItem = DecadeListBox.ItemContainerGenerator.ContainerFromItem(selectedItem) as ListBoxItem;
+
+            if (selectedListBoxItem != null)
+            {
+                int decade = Convert.ToInt32(selectedListBoxItem.Content);
+                for (int i = 1; i < 11; ++i)
+                {
+                    BabyNameListBox.Items.Add($"{i,2} {_rankMatrix[decade, i - 1]}");
+                }
+            }
+        }
+
+
+
+
+        private void AddBabyNamesToArray()
+        {
+            try
             {
                 using (var sr = new StreamReader("BabyNames.txt"))
                 {
                     while (!sr.EndOfStream)
                     {
                         string line = sr.ReadLine();
-                        var instans = new BabyName(line);
-                        _babyNames.Add(instans);
-                        //var item = new ListBoxItem();
-                        //item.Content = line;
-                        //ResultListBox.Items.Add(item);
+                        _babyNames.Add(new BabyName(line));
                     }
                 }
             }
@@ -67,4 +88,33 @@ namespace BabyNames
                 };
                 BabyNameListBox.Items.Add(item);
             }
- */
+        }
+        
+        private void SearchBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            SearchListBox.Items.Clear();
+            string searchedName = NameTextBox.Text;
+            var found = false;
+            foreach (var babyName in _babyNames)
+            {
+                if (babyName.Name.Equals(searchedName))
+                {
+                    found = true;
+                    //TODO add Year and Rank to SearchListBox
+                    for (int i = 1900; i <= 2000; i+=10)
+                    {
+                        SearchListBox.Items.Add($" {i}  {babyName.Rank(i), 11}");
+                    }
+
+                    RankTextBox.Text = babyName.AverageRank().ToString();
+
+                    TrendTextBox.Text = babyName.Trend().ToString();
+                }
+            }
+            if (!found)
+            {
+                    MessageBox.Show("Name does not exist in the statistic");
+            }
+        }
+    }
+}
